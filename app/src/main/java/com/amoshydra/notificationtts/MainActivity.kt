763391ab4c30377b1988fff516b9bc625826a43c
Amos Wong <amoswwj@gmail.com>
2019-11-05 01:19:53 +0800

    feat: Implement notification access setting check

--- app/src/main/java/com/amoshydra/notificationtts/MainActivity.kt
+++ app/src/main/java/com/amoshydra/notificationtts/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.amoshydra.notificationtts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.content.ComponentName
+import android.content.Context
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (hasEnabledNotificationListener(applicationContext)) {
+            startService(Intent(this, NotificationListener::class.java))
+        } else {
+            val intent = Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS")
+            startActivity(intent)
+        }
+    }
+
+    private fun hasEnabledNotificationListener(context: Context): Boolean {
+        val componentName = ComponentName(context, NotificationListener::class.java)
+
+        val enabledListenerString = Settings.Secure.getString(
+            applicationContext.contentResolver,
+            "enabled_notification_listeners"
+        )
+
+        return enabledListenerString?.contains(componentName.flattenToString()) ?: false
     }
 }
